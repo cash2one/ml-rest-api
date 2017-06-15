0c409ee395023073d15f47e45f53e0cc41e75404
Trent Resh <trentresh@Trents-MBP.fios-router.home>
2017-06-15 15:48:51 -0700

    add fix

--- server.py
+++ server.py
@@ -62,6 +62,7 @@ challenger_players_stats = clean_and_extract_stats(challenger_players_data)
 bronze_players_stats = clean_and_extract_stats(bronze_players_data)
 
 least_games = len(challenger_players_stats)
+bronze_least_games = len(bronze_players_stats)
 
 challenger_gold_earned = [challenger_players_stats[i]["goldEarned"]for i in range(least_games) if "goldEarned" in challenger_players_stats[i]]
 challenger_gold_spent = [challenger_players_stats[i]["goldSpent"]for i in range(least_games) if "goldSpent" in challenger_players_stats[i]]
@@ -73,15 +74,15 @@ challenger_wins = [record_wins(challenger_players_stats[i]["win"]) for i in rang
 challenger_kills = [challenger_players_stats[i]["championsKilled"] for i in range(least_games) if "championsKilled" in challenger_players_stats[i]]
 challenger_deaths = [challenger_players_stats[i]["numDeaths"] for i in range(least_games) if "numDeaths" in challenger_players_stats[i]]
 
-bronze_gold_earned = [bronze_players_stats[i]["goldEarned"]for i in range(least_games) if "goldEarned" in bronze_players_stats[i]]
-bronze_gold_spent = [bronze_players_stats[i]["goldSpent"]for i in range(least_games) if "goldSpent" in bronze_players_stats[i]]
-bronze_time = [bronze_players_stats[i]["timePlayed"]for i in range(least_games)]
-bronze_dmg_taken = [bronze_players_stats[i]["totalDamageTaken"]for i in range(least_games)]
-bronze_neutral_minions = [bronze_players_stats[i]["minionsKilled"] for i in range(least_games) if "minionsKilled" in bronze_players_stats[i]]
-bronze_minions = [bronze_players_stats[i]["neutralMinionsKilled"] for i in range(least_games) if "neutralMinionsKilled" in bronze_players_stats[i]]
-bronze_wins = [record_wins(bronze_players_stats[i]["win"]) for i in range(least_games)]
-bronze_kills = [bronze_players_stats[i]["championsKilled"] for i in range(least_games) if "championsKilled" in bronze_players_stats[i]]
-bronxe_deaths = [bronze_players_stats[i]["numDeaths"] for i in range(least_games) if "numDeaths" in bronze_players_stats[i]]
+bronze_gold_earned = [bronze_players_stats[i]["goldEarned"]for i in range(bronze_least_games) if "goldEarned" in bronze_players_stats[i]]
+bronze_gold_spent = [bronze_players_stats[i]["goldSpent"]for i in range(bronze_least_games) if "goldSpent" in bronze_players_stats[i]]
+bronze_time = [bronze_players_stats[i]["timePlayed"]for i in range(bronze_least_games)]
+bronze_dmg_taken = [bronze_players_stats[i]["totalDamageTaken"]for i in range(bronze_least_games)]
+bronze_neutral_minions = [bronze_players_stats[i]["minionsKilled"] for i in range(bronze_least_games) if "minionsKilled" in bronze_players_stats[i]]
+bronze_minions = [bronze_players_stats[i]["neutralMinionsKilled"] for i in range(bronze_least_games) if "neutralMinionsKilled" in bronze_players_stats[i]]
+bronze_wins = [record_wins(bronze_players_stats[i]["win"]) for i in range(bronze_least_games)]
+bronze_kills = [bronze_players_stats[i]["championsKilled"] for i in range(bronze_least_games) if "championsKilled" in bronze_players_stats[i]]
+bronxe_deaths = [bronze_players_stats[i]["numDeaths"] for i in range(bronze_least_games) if "numDeaths" in bronze_players_stats[i]]
 
 
 
