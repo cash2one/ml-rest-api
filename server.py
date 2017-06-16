4187eb365ac141670bb56781ecd120b26830462a
Trent Resh <trentresh@Trents-MBP.fios-router.home>
2017-06-16 14:05:58 -0700

    add fix

--- server.py
+++ server.py
@@ -240,7 +240,7 @@ def logisitic_regression():
     x = x.transpose()
     clf = linear_model.LogisticRegression(C=1e5)
     clf.fit(x, y)
-    player_data = np.matrix(request.json["x"])
+    player_data = np.matrix([request.json["x"], request.json["y"]])
     player_data = player_data.transpose()
 
     data = {
