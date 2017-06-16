ad6b0ead5234b9c9ec95698a80ce15331c2ad281
Trent Resh <trentresh@Trents-MBP.fios-router.home>
2017-06-16 15:18:29 -0700

    add fix

--- server.py
+++ server.py
@@ -230,17 +230,19 @@ def logisitic_regression():
     if not request.json:
         abort(400)
 
-    cge = challenger_gold_earned
-    cgs = challenger_gold_spent
-
+    # cge = challenger_gold_earned
+    # cgs = challenger_gold_spent
+    cgt = challenger_time
+    ck = challenger_kills
+    cm = challenger_minions
     wins = challenger_wins
     cd = challenger_deaths
-    x = np.matrix([cge, cgs])
+    x = np.matrix([ck, cm, cgt])
     y = np.array(wins)
     x = x.transpose()
     clf = linear_model.LogisticRegression(C=1e5)
     clf.fit(x, y)
-    player_data = np.matrix([request.json["x"], request.json["y"]])
+    player_data = np.matrix([request.json["x"], request.json["y"], request.json["z"]])
     player_data = player_data.transpose()
     data = {
         "data": list(clf.predict(player_data))
