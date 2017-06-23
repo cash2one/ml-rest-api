2de81f19727f828aa8b12191921890c85bd4cc90
Trent Resh <trentresh@Trents-MacBook-Pro.local>
2017-06-23 11:33:06 -0700

    testing logistic regression

--- server.py
+++ server.py
@@ -263,7 +263,7 @@ def support_vector_machine():
     x = np.matrix([cge, cgs, cgt])
     y = np.array(wins)
     x = x.transpose()
-    clf = svm.SVC(C=1e5, tol=1e-10, cache_size=600, kernel='linear',
+    clf = svm.SVC(C=1.0, tol=1e-10, cache_size=600, kernel='linear',
               class_weight='auto')
     clf.fit(x, wins)
     player_data = np.matrix([request.json["x"], request.json["y"], request.json["z"]])
