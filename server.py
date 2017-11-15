ad8c6bf1cc420d49bf8b72d9bc44bdcba781aad5
Trent Resh <trentresh@Trents-MBP.fios-router.home>
2017-11-15 15:18:32 -0800

    changes from str to int'

--- server.py
+++ server.py
@@ -433,22 +433,22 @@ def integrate_():
 
     print request.json["data"]
 
-    # points = np.array.(points).astype(np.int32)
-    # print points
-    # points = points.astype(np.int32)
-    # x = points[:,0]
-    # y = points[:,1]
-    #
-    # # calculate polynomial
-    # z = np.polyfit(x, y, 2)
-    # f = np.poly1d(z)
-    # print f
-    # def integrand(x, a, b, c):
-    #     return (a*x**2) + (b * x) + c
-    # I = quad(integrand, 1, 5, args=(f[2], f[1], f[0]))
+    points = np.array.(points).astype(np.int32)
+    print points
+    points = points.astype(np.int32)
+    x = points[:,0]
+    y = points[:,1]
+
+    # calculate polynomial
+    z = np.polyfit(x, y, 2)
+    f = np.poly1d(z)
+    print f
+    def integrand(x, a, b, c):
+        return (a*x**2) + (b * x) + c
+    I = quad(integrand, 1, 5, args=(f[2], f[1], f[0]))
     data = {
-        # "data": I[0] * 000.1
-        "data": 200
+        "data": I[0] * 000.1
+        # "data": 200
     }
 
     return jsonify(data), 201
